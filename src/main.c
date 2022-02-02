596137161dcd0651e79210fbfd0bee34835e0911
glennfrey <93269547+glennfrey@users.noreply.github.com>
2022-02-02 22:10:34 +0800

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,21 @@
+/**
+ * @file main.c
+ * 
+ * @author Glenn Frey Olamit
+ * @date 2/2/2022
+ *
+ */
+
+#include "../include/common/course1.h"
+#include "../include/common/memory.h"
+#include "../include/common/stats.h"
+#include "../include/common/data.h"
+#include "../include/common/platform.h"
+
+void main (void){
+
+    #ifdef COURSE1
+    course1();
+    #endif
+}
+
